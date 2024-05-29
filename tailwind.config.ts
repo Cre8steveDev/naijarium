752895b5b1d61fd1a95c2f0b4e80dae612527149
Stephen Omoregie (Win) <cr8stevestudios@gmail.com>
2024-05-29 07:37:26 +0100

    Desktop stash

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      screens: {
+        'max-h-768': { raw: '(max-height: 769px)' },
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
